f3afdd4f3da937974c1de7dc828d69555a7c7c74
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-12-30 23:44:03 +0300

    refactor: removeimage class atrribute

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,6 @@ export default function Home() {
             src={modelBoy}
             alt="s"
             priority={true}
-            className=""
           />
         </div>
         <h1 className="pt-24 md:pt-0 font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
