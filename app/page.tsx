997513c5c561a23c1d51d5b9e2d6253af7394f7a
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-12-30 23:42:09 +0300

    style: finally make the hero section responsive on all screens

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import heroDesign from '@/public/hero/Hero Design.png';
 import featuredItem from '@/public/hero/Hero Featured Item.png';
+import mobieHeroDesign from '@/public/hero/Hero Mobile Design.png';
 import modelBoy from '@/public/hero/Model Boy.png';
 import modelGirl from '@/public/hero/Model Girl.png';
 import sitwideSaleRing from '@/public/hero/Sitewide Sale.png';
@@ -12,40 +13,79 @@ import sitwideSaleRing from '@/public/hero/Sitewide Sale.png';
 export default function Home() {
   return (
     <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:pt-32">
-      <div>
-        <Image src={heroDesign} alt="." className="absolute left-0 top-28" />
+      {/* Mobile Hero Design sm screens */}
+      <div className="absolute top-[0.5rem] md:hidden">
+        <Image src={mobieHeroDesign} alt="#" priority={true} />
       </div>
-      <div>
+
+      {/* Tablet and larger Hero design and Featured Item md screens */}
+      <div className="hidden absolute top-[0.5rem] md:block md:-left-[4rem] lg:left-0">
+        <Image src={heroDesign} alt="#" priority={true} />
+      </div>
+      <div className="hidden absolute top-[5rem] md:block md:right-[1rem] lg:right-[4rem] xl:right-[8rem]">
         <Image
+          width={184}
+          height={184}
           src={featuredItem}
-          alt="."
-          className="absolute right-16 top-28"
+          alt="#"
+          priority={true}
         />
       </div>
 
       <div className="container relative flex max-w-[64rem] flex-col items-center gap-4 text-center">
-        <div>
+        {/* Tablet and Larger Display for the hero image- modelBoy  */}
+        <div className="hidden absolute left-2 bottom-0 md:block lg:hidden">
           <Image
+            width={200}
+            height={271}
             src={modelBoy}
-            alt="."
-            className="absolute left-[-12rem] bottom-[10rem]"
+            alt="s"
+            priority={true}
+            className="md:w-[200px] md:h-[271px] lg:w-[284px] h-[345px]"
           />
         </div>
-        <div>
+        <div className="hidden absolute -left-[0.09rem] xl:-left-10 2xl:-left-20 bottom-0 lg:block">
           <Image
-            src={modelGirl}
-            alt="."
-            className="absolute right-[-16rem] bottom-[7rem]"
+            width={290}
+            height={450}
+            src={modelBoy}
+            alt="s"
+            priority={true}
+            className=""
           />
         </div>
-        <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+        <h1 className="pt-24 md:pt-0 font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
           Originality <br />
-          You Wear <br /> Everday
+          You Wear <br /> Everyday
         </h1>
-        <div className="flex items-center space-x-4">
+        {/* Tablet and Larger Display for the hero image- modelGirl  */}
+        <div className="hidden absolute -right-5 -bottom-[2rem] md:block lg:hidden">
+          <Image
+            width={300.957}
+            height={418.552}
+            src={modelGirl}
+            alt="s"
+            priority={true}
+          />
+        </div>
+        {/* Tablet and Larger Display for the hero image- modelGirl  */}
+        <div className="hidden absolute -right-9 xl:-right-20 2xl:-right-40 -bottom-[2rem] lg:block">
+          <Image
+            width={420}
+            height={533}
+            src={modelGirl}
+            alt="s"
+            priority={true}
+          />
+        </div>
+
+        <div className="flex items-center space-x-4 mt-4 md:mt-0">
           <Link
             href="/products"
-            className={cn(buttonVariants({ size: 'lg' }), 'bg-primary')}
+            className={cn(
+              buttonVariants({ size: 'lg' }),
+              'bg-primary shadow-lg'
+            )}
           >
             Shop Now
           </Link>
@@ -56,13 +96,13 @@ export default function Home() {
                 size: 'lg',
                 variant: 'outline',
               }),
-              'border border-primary text-primary'
+              'border border-primary text-primary shadow-lg'
             )}
           >
             Flash Sales
           </Link>
         </div>
-        <div className="font-sans_bold font-bold text-6xl pt-6 md:pt-10 lg:pt-28 realtive">
+        <div className="font-sans_bold font-bold text-2xl md:text-4xl lg:text-6xl pt-10 lg:pt-28">
           <p className="max-w-[42rem]">
             SITEWIDE <br />
             SALE
@@ -70,7 +110,36 @@ export default function Home() {
           <Image
             src={sitwideSaleRing}
             alt="S"
-            className="relative top-[-11rem]"
+            priority={true}
+            className="relative top-[-6rem] md:top-[-8rem] lg:top-[-11rem] w-[14.375rem] md:w-[20rem] lg:w-[26rem]"
+          />
+        </div>
+        {/* Mobile Display for the Hero images  */}
+        <div
+          className="flex flex-col justify-center items-center relative 
+        gap-y-2 -top-32 md:hidden "
+        >
+          <Image
+            width={223}
+            height={271}
+            src={modelBoy}
+            alt="s"
+            priority={true}
+          />
+          <Image
+            width={194.4}
+            height={194.4}
+            src={featuredItem}
+            priority={true}
+            alt="s"
+            className="mt-8"
+          />
+          <Image
+            width={399.957}
+            height={418.552}
+            src={modelGirl}
+            priority={true}
+            alt="s"
           />
         </div>
       </div>
