e7d5bb7de3845cdbabec2c848d51357599b5ad8e
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-12-30 13:19:44 +0300

    chore: create site config

---
+++ config/site.ts
@@ -0,0 +1,15 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: 'Shoply',
+  description: 'Your everday wear for work, home, travel and sports',
+  footer: [
+    { name: 'Home', href: '/' },
+    { name: 'About', href: '#' },
+    { name: 'Products', href: '#' },
+    { name: 'Sale', href: '#' },
+    { name: 'Privacy Policy', href: '#' },
+    { name: 'Terms & Conditions', href: '#' },
+    { name: 'FAQ', href: '#' },
+  ],
+};
