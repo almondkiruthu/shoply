fc3c5d48a89905917cfd3ff164eff87b35a064d5
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-12-31 13:21:09 +0300

    style: add assets

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,12 @@
-import { createClient } from 'next-sanity'
+import { createClient } from 'next-sanity';
 
-import { apiVersion, dataset, projectId, useCdn } from '../env'
+import { apiVersion, dataset, projectId, useCdn } from '../env';
 
 export const client = createClient({
   apiVersion,
   dataset,
   projectId,
   useCdn,
-})
+  token:
+    'skb9ugCWwpWrO4RwNQcaoDKp7qzpEhG03MpmsM1JCdDOaCfAJMsREceYKRH89hUDd1B0O4Ucmwp1RVQ9d2IT9gCxGhokXfpmxwv13C0zaIGObLPrZDDesL6n8lrKFz17fXFg5R4KSyzWroZACZbR29cvtcEu55vjFDOxknSHO5hX4KkS6SjE',
+});
