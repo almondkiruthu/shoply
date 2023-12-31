2e69b243909f80a327b6346774e7126eb37d7f87
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-12-31 13:22:15 +0300

    refactor: fix sanity client

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,6 +7,4 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:
-    'skb9ugCWwpWrO4RwNQcaoDKp7qzpEhG03MpmsM1JCdDOaCfAJMsREceYKRH89hUDd1B0O4Ucmwp1RVQ9d2IT9gCxGhokXfpmxwv13C0zaIGObLPrZDDesL6n8lrKFz17fXFg5R4KSyzWroZACZbR29cvtcEu55vjFDOxknSHO5hX4KkS6SjE',
 });
