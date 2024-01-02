95bfcb98abf18e056b0095bbe16c113f9cf57d1c
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-01-02 21:25:30 +0300

    feat: add carousel to popular items

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import PopularItems from '@/components/popular-items';
 import { buttonVariants } from '@/components/ui/button';
+import { popularItems } from '@/config/popular';
 import { cn } from '@/lib/utils';
 import heroDesign from '@/public/hero/Hero Design.png';
 import featuredItem from '@/public/hero/Hero Featured Item.png';
@@ -12,136 +14,139 @@ import sitwideSaleRing from '@/public/hero/Sitewide Sale.png';
 
 export default async function Home() {
   return (
-    <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:pt-32">
-      {/* Mobile Hero Design sm screens */}
-      <div className="absolute top-[0.5rem] md:hidden">
-        <Image src={mobieHeroDesign} alt="#" priority={true} />
-      </div>
-
-      {/* Tablet and larger Hero design and Featured Item md screens */}
-      <div className="hidden absolute top-[0.5rem] md:block md:-left-[4rem] lg:left-0">
-        <Image src={heroDesign} alt="#" priority={true} />
-      </div>
-      <div className="hidden absolute top-[5rem] md:block md:right-[1rem] lg:right-[4rem] xl:right-[8rem]">
-        <Image
-          width={184}
-          height={184}
-          src={featuredItem}
-          alt="#"
-          priority={true}
-        />
-      </div>
-
-      <div className="container relative flex max-w-[64rem] flex-col items-center gap-4 text-center">
-        {/* Tablet and Larger Display for the hero image- modelBoy  */}
-        <div className="hidden absolute left-2 bottom-0 md:block lg:hidden">
-          <Image
-            width={200}
-            height={271}
-            src={modelBoy}
-            alt="s"
-            priority={true}
-            className="md:w-[200px] md:h-[271px] lg:w-[284px] h-[345px]"
-          />
-        </div>
-        <div className="hidden absolute -left-[0.09rem] xl:-left-10 2xl:-left-20 bottom-0 lg:block">
-          <Image
-            width={290}
-            height={450}
-            src={modelBoy}
-            alt="s"
-            priority={true}
-          />
-        </div>
-        <h1 className="pt-24 md:pt-0 font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
-          Originality <br />
-          You Wear <br /> Everyday
-        </h1>
-        {/* Tablet and Larger Display for the hero image- modelGirl  */}
-        <div className="hidden absolute -right-5 -bottom-[2rem] md:block lg:hidden">
-          <Image
-            width={300.957}
-            height={418.552}
-            src={modelGirl}
-            alt="s"
-            priority={true}
-          />
-        </div>
-        {/* Tablet and Larger Display for the hero image- modelGirl  */}
-        <div className="hidden absolute -right-9 xl:-right-20 2xl:-right-40 -bottom-[2rem] lg:block">
-          <Image
-            width={420}
-            height={533}
-            src={modelGirl}
-            alt="s"
-            priority={true}
-          />
+    <>
+      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:pt-32">
+        {/* Mobile Hero Design sm screens */}
+        <div className="absolute top-[0.5rem] md:hidden">
+          <Image src={mobieHeroDesign} alt="#" priority={true} />
         </div>
 
-        <div className="flex items-center space-x-4 mt-4 md:mt-0">
-          <Link
-            href="/products"
-            className={cn(
-              buttonVariants({ size: 'lg' }),
-              'bg-primary shadow-lg'
-            )}
-          >
-            Shop Now
-          </Link>
-          <Link
-            href="/sales"
-            className={cn(
-              buttonVariants({
-                size: 'lg',
-                variant: 'outline',
-              }),
-              'border border-primary text-primary shadow-lg'
-            )}
-          >
-            Flash Sales
-          </Link>
+        {/* Tablet and larger Hero design and Featured Item md screens */}
+        <div className="hidden absolute top-[0.5rem] md:block md:-left-[4rem] lg:left-0">
+          <Image src={heroDesign} alt="#" priority={true} />
         </div>
-        <div className="font-sans_bold font-bold text-2xl md:text-4xl lg:text-6xl pt-10 lg:pt-28">
-          <p className="max-w-[42rem]">
-            SITEWIDE <br />
-            SALE
-          </p>
+        <div className="hidden absolute top-[5rem] md:block md:right-[1rem] lg:right-[4rem] xl:right-[8rem]">
           <Image
-            src={sitwideSaleRing}
-            alt="S"
-            priority={true}
-            className="relative top-[-6rem] md:top-[-8rem] lg:top-[-11rem] w-[14.375rem] md:w-[20rem] lg:w-[26rem]"
-          />
-        </div>
-        {/* Mobile Display for the Hero images  */}
-        <div
-          className="flex flex-col justify-center items-center relative 
-        gap-y-2 -top-32 md:hidden "
-        >
-          <Image
-            width={223}
-            height={271}
-            src={modelBoy}
-            alt="s"
-            priority={true}
-          />
-          <Image
-            width={194.4}
-            height={194.4}
+            width={184}
+            height={184}
             src={featuredItem}
+            alt="#"
             priority={true}
-            alt="s"
-            className="mt-8"
-          />
-          <Image
-            width={399.957}
-            height={418.552}
-            src={modelGirl}
-            priority={true}
-            alt="s"
           />
         </div>
-      </div>
-    </section>
+
+        <div className="container relative flex max-w-[64rem] flex-col items-center gap-4 text-center">
+          {/* Tablet and Larger Display for the hero image- modelBoy  */}
+          <div className="hidden absolute left-2 bottom-0 md:block lg:hidden">
+            <Image
+              width={200}
+              height={271}
+              src={modelBoy}
+              alt="s"
+              priority={true}
+              className="md:w-[200px] md:h-[271px] lg:w-[284px] h-[345px]"
+            />
+          </div>
+          <div className="hidden absolute -left-[0.09rem] xl:-left-10 2xl:-left-20 bottom-0 lg:block">
+            <Image
+              width={290}
+              height={450}
+              src={modelBoy}
+              alt="s"
+              priority={true}
+            />
+          </div>
+          <h1 className="pt-24 md:pt-0 font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+            Originality <br />
+            You Wear <br /> Everyday
+          </h1>
+          {/* Tablet and Larger Display for the hero image- modelGirl  */}
+          <div className="hidden absolute -right-5 -bottom-[2rem] md:block lg:hidden">
+            <Image
+              width={300.957}
+              height={418.552}
+              src={modelGirl}
+              alt="s"
+              priority={true}
+            />
+          </div>
+          {/* Tablet and Larger Display for the hero image- modelGirl  */}
+          <div className="hidden absolute -right-9 xl:-right-20 2xl:-right-40 -bottom-[2rem] lg:block">
+            <Image
+              width={420}
+              height={533}
+              src={modelGirl}
+              alt="s"
+              priority={true}
+            />
+          </div>
+
+          <div className="flex items-center space-x-4 mt-4 md:mt-0">
+            <Link
+              href="/products"
+              className={cn(
+                buttonVariants({ size: 'lg' }),
+                'bg-primary shadow-lg'
+              )}
+            >
+              Shop Now
+            </Link>
+            <Link
+              href="/sales"
+              className={cn(
+                buttonVariants({
+                  size: 'lg',
+                  variant: 'outline',
+                }),
+                'border border-primary text-primary shadow-lg'
+              )}
+            >
+              Flash Sales
+            </Link>
+          </div>
+          <div className="font-sans_bold font-bold text-2xl md:text-4xl lg:text-6xl pt-10 lg:pt-28">
+            <p className="max-w-[42rem]">
+              SITEWIDE <br />
+              SALE
+            </p>
+            <Image
+              src={sitwideSaleRing}
+              alt="S"
+              priority={true}
+              className="relative top-[-6rem] md:top-[-8rem] lg:top-[-11rem] w-[14.375rem] md:w-[20rem] lg:w-[26rem]"
+            />
+          </div>
+          {/* Mobile Display for the Hero images  */}
+          <div
+            className="flex flex-col justify-center items-center relative 
+        gap-y-2 -top-32 md:hidden "
+          >
+            <Image
+              width={223}
+              height={271}
+              src={modelBoy}
+              alt="s"
+              priority={true}
+            />
+            <Image
+              width={194.4}
+              height={194.4}
+              src={featuredItem}
+              priority={true}
+              alt="s"
+              className="mt-8"
+            />
+            <Image
+              width={399.957}
+              height={418.552}
+              src={modelGirl}
+              priority={true}
+              alt="s"
+            />
+          </div>
+        </div>
+      </section>
+      <PopularItems products={popularItems} />
+    </>
   );
 }
--- components/popular-items.tsx
+++ components/popular-items.tsx
@@ -3,12 +3,19 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { formatCurrencyString } from 'use-shopping-cart';
 
-import { CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
-import { SanityProduct } from '@/config/inventory';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
 import { shimmer, toBase64 } from '@/lib/image';
 import { urlForImage } from '@/sanity/lib/image';
+import { PopularItems } from '@/types';
+
 interface PopularItemsProps {
-  products: SanityProduct[];
+  products: PopularItems;
 }
 
 const PopularItems = ({ products }: PopularItemsProps) => {
@@ -18,42 +25,47 @@ const PopularItems = ({ products }: PopularItemsProps) => {
         id="popular"
         className="container space-y-6 bg-slate-50 py-8 md:py-12 lg:pt-20 lg:pb-18"
       >
-        <div className="mx-auto flex max-w-[58rem] items-center space-y-4">
+        <div className="mx-auto flex max-w-[58rem] items-center space-y-4 relative">
           <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
             Popular Items
           </h2>
         </div>
-        {products.map((product) => (
-          <Link
-            key={product._id}
-            href={`/products/${product.slug}`}
-            className="group text-sm"
-          >
-            <div
-              className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg
+        <Carousel>
+          <CarouselContent>
+            {products?.map((product) => (
+              <>
+                <CarouselItem>
+                  <Link
+                    key={product.title}
+                    href={'#'}
+                    className="group text-sm"
+                  >
+                    <div
+                      className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg
             borde-2 border-gray-200 bg-gray-100 group-hover:opacity-75"
-            >
-              <Image
-                placeholder="blur"
-                blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                  shimmer(225, 280)
-                )}`}
-                src={urlForImage(product.images[0])}
-                alt={product.name}
-                width={225}
-                height={280}
-                className="h-full w-full object-cover object-center"
-              />
-            </div>
-            <h3 className="mt-4 font-medium">{product.name}</h3>
-            <p className="mt-2 font-medium">
-              {formatCurrencyString({
-                currency: product.currency,
-                value: product.price,
-              })}
-            </p>
-          </Link>
-        ))}
+                    >
+                      <Image
+                        placeholder="blur"
+                        blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                          shimmer(225, 280)
+                        )}`}
+                        src={product.image}
+                        alt={product.title}
+                        width={225}
+                        height={280}
+                        className="h-full w-full object-cover object-center"
+                      />
+                    </div>
+                    <h3 className="mt-4 font-medium">{product.title}</h3>
+                    <p className="mt-2 font-medium">{product.price}</p>
+                  </Link>
+                </CarouselItem>
+              </>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </section>
     </>
   );
