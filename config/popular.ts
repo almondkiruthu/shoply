63ed0afb932bfaa2774d41a4c0d74f9d489cf894
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-01-03 20:23:21 +0300

    fix: add unique id on the list components

--- config/popular.ts
+++ config/popular.ts
@@ -8,36 +8,42 @@ import { PopularItems } from '@/types';
 
 export const popularItems: PopularItems = [
   {
+    id: 'efr3',
     image: denimJacket,
     title: 'Denim Jacket',
     price: 2000,
     sizes: ['s', 'm', 'l', 'xl'],
   },
   {
+    id: 'fgfgfg',
     image: shirt,
     title: 'Shirt',
     price: 1000,
     sizes: ['s', 'm', 'l', 'xl'],
   },
   {
+    id: 'sedr',
     image: shorts,
     title: 'Shorts',
     price: 800,
     sizes: ['s', 'm', 'l', 'xl'],
   },
   {
+    id: 'rt4566',
     image: camisole,
     title: 'Camisole',
     price: 800,
     sizes: ['s', 'm', 'l', 'xl'],
   },
   {
+    id: 'fgfg5665',
     image: cropTop,
     title: 'Crop Top',
     price: 900,
     sizes: ['s', 'm', 'l', 'xl'],
   },
   {
+    id: 'fgffgy455789',
     image: sleeveLessBlouse,
     title: 'Sleeveless Blouse',
     price: 1000,
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,7 @@
 import { StaticImageData } from 'next/image';
 
 export type PopularItem = {
+  id: string;
   image: StaticImageData;
   title: string;
   price: number;
