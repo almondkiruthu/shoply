c2bf370aebed97cf38a59d69078c23598b33ad03
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-01-03 18:44:51 +0300

    style: create next and prev button for carousel

--- components/popular-items.tsx
+++ components/popular-items.tsx
@@ -12,10 +12,11 @@ import {
 } from '@/components/ui/carousel';
 import { formatPrice } from '@/lib/format';
 import { shimmer, toBase64 } from '@/lib/image';
+import { cn } from '@/lib/utils';
 import { urlForImage } from '@/sanity/lib/image';
 import { PopularItems } from '@/types';
 
-import { Button } from './ui/button';
+import { Button, buttonVariants } from './ui/button';
 import { Icons } from './icons';
 
 interface PopularItemsProps {
@@ -27,67 +28,85 @@ const PopularItems = ({ products }: PopularItemsProps) => {
     <>
       <section
         id="popular"
-        className="container space-y-6 bg-slate-50/10 py-8 md:py-12 lg:pt-20 lg:pb-18"
+        className="space-y-6 bg-slate-50/70 py-8 md:py-12 lg:pt-20 lg:pb-18"
       >
-        <div className="mx-auto flex items-center space-y-4 relative">
-          <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
-            Popular Items
-          </h2>
-        </div>
-        <Carousel>
-          <CarouselContent>
-            {products?.map((product) => (
-              <>
-                <CarouselItem className='basis-1/4 pl-4'>
-                  <Link key={product.title} href={'#'} className="">
-                    <div className="w-[300px] rounded-lg shadow-lg bg-white space-y-2">
-                      <div
-                        className="flex flex-col items-center justify-center
+        <div className="container relative">
+          <div className="mx-auto flex items-center space-y-4">
+            <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-5xl pb-4">
+              Popular Items
+            </h2>
+          </div>
+          <Carousel>
+            <CarouselContent>
+              {products?.map((product) => (
+                <>
+                  <CarouselItem className="basis-1/1 xl:basis-1/4 pl-4">
+                    <Link key={product.title} href={'#'} className="">
+                      <div className="w-[300px] rounded-lg shadow-lg bg-white space-y-2">
+                        <div
+                          className="flex flex-col items-center justify-center
                       p-5"
-                      >
-                        <Image src={product.image} alt={product.title} />
-                      </div>
-                      <div
-                        className="flex flex-col text-left
-                      gap-y-2 ml-4"
-                      >
-                        <h3 className="font-sans_bold font-bold text-xl tracking-tight">
-                          {product.title}
-                        </h3>
-                        <p className="text-sm font-normal leading-normal">
-                          {formatPrice(product.price)}
-                        </p>
-                      </div>
-                      <div className="flex items-center gap-x-5 ml-4">
-                        {product.sizes.map((size, index) => (
-                          <Button
-                            key={index}
-                            variant="outline"
-                            size="icon"
-                            className="border-primary text-primary"
-                          >
-                            <p>{size.toUpperCase()}</p>
-                          </Button>
-                        ))}
-                      </div>
-                      <div className="ml-4 flex items-center pt-4">
-                        <Button className="">Add to Cart</Button>
-                        <Button
-                          className="rounded-full ml-auto mr-4"
-                          size="icon"
                         >
-                          <Icons.heart className="w-4 h-4" />
-                        </Button>
+                          <Image src={product.image} alt={product.title} />
+                        </div>
+                        <div
+                          className="flex flex-col text-left
+                      gap-y-2 ml-4"
+                        >
+                          <h3 className="font-sans_bold font-bold text-xl tracking-tight">
+                            {product.title}
+                          </h3>
+                          <p className="text-sm font-normal leading-normal">
+                            {formatPrice(product.price)}
+                          </p>
+                        </div>
+                        <div className="flex items-center gap-x-5 ml-4">
+                          {product.sizes.map((size, index) => (
+                            <Button
+                              key={index}
+                              variant="outline"
+                              size="icon"
+                              className="border-primary text-primary"
+                            >
+                              <p>{size.toUpperCase()}</p>
+                            </Button>
+                          ))}
+                        </div>
+                        <div className="ml-4 flex items-center pt-4">
+                          <Button className="">Add to Cart</Button>
+                          <Button
+                            className="rounded-full ml-auto mr-4"
+                            size="icon"
+                          >
+                            <Icons.heart className="w-4 h-4" />
+                          </Button>
+                        </div>
                       </div>
-                    </div>
-                  </Link>
-                </CarouselItem>
-              </>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
+                    </Link>
+                  </CarouselItem>
+                </>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious
+              className={cn(
+                buttonVariants({
+                  size: 'icon',
+                  variant: 'default',
+                }),
+                'rounded-full hover:text-white shadow-lg absolute lg:-top-12 left-[13rem] lg:left-[80rem]'
+              )}
+            />
+            <CarouselNext
+              className={cn(
+                buttonVariants({
+                  size: 'icon',
+                  variant: 'default',
+                }),
+                'rounded-full hover:text-white shadow-lg absolute lg:-top-12 -right-7 lg:-right-6'
+              )}
+            />
+          </Carousel>
+        </div>
       </section>
     </>
   );
