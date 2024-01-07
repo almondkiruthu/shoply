0eb79e2216fcec9a42b05353b15ba3e6cc24c506
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-01-07 21:22:24 +0300

    chore: syntactically edited the footer component

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,5 +1,7 @@
-const SiteFooter = () => {
-  return <div>Hey I&apos;m site footer</div>;
+interface SiteFooter extends React.HTMLAttributes<HTMLDivElement> {}
+
+const SiteFooter = ({}: SiteFooter) => {
+  return <footer>Hey I&apos;m site footer</footer>;
 };
 
 export default SiteFooter;
