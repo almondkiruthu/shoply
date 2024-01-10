64fa35654175fc466833969920db9936d338bc4f
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-01-10 21:47:12 +0300

    chore: trieed to fix footer display error

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -5,15 +5,15 @@ import { cn } from '@/lib/utils';
 const SiteFooter = ({ className }: React.HTMLAttributes<HTMLElement>) => {
   return (
     <footer className={cn(className)}>
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+      <div className="container flex flex-col items-center gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col">
           <Icons.mainLogo />
-          <div className="flex items-center">
+          <div className="flex items-center justify-between">
             <div className="flex flex-col -space-y-1 text-base">
               <p>Discover Your Distinctive Look: </p>
               <p>Fashioned with Precision, Worn with Confidence</p>
             </div>
-            <div className="flex gap-x-3 items-center ml-auto">
+            <div className="flex gap-x-3 items-center">
               {siteConfig.footer.map((link, index) => (
                 <a key={index} className="text-sm leading-loose">
                   {link.name}
