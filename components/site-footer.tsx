7644a28812623415c9d63f09399af95c8b9e9080
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-01-10 21:19:06 +0300

    style: create footer p1

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,10 +1,18 @@
+import { Icons } from '@/components/icons';
 import { cn } from '@/lib/utils';
 
 const SiteFooter = ({ className }: React.HTMLAttributes<HTMLElement>) => {
   return (
     <footer className={cn(className)}>
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0"
-      ></div>
+      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+        <div className="flex flex-col">
+          <Icons.mainLogo />
+          <div className='flex flex-col gap-y-1'>
+            <p>Discover Your Distinctive Look: </p>
+            <p>Fashioned with Precision, Worn with Confidence</p>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
