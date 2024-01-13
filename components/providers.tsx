05571380e3b5907df092ba164f3397e55c6ff0b2
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-01-13 19:06:34 +0300

    refactor: increase listeners

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,11 +1,12 @@
-import { TailwindIndicator } from '@/components/tailwind-indicator';
-import { Toaster } from '@/components/ui/toaster';
+import { TailwindIndicator } from "@/components/tailwind-indicator";
+import { Toaster } from "@/components/ui/toaster";
 
 interface ProvidersProps {
   children: React.ReactNode;
 }
 
 const Providers = ({ children }: ProvidersProps) => {
+  process.setMaxListeners(15);
   return (
     <>
       <Toaster />
