53ff0c0d007e4774522ae39f93ddd2f6fc409371
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-01-16 19:37:25 +0300

    chore: create products page

---
+++ app/(products)/products/page.tsx
@@ -0,0 +1,9 @@
+const ProductPage = () => {
+  return (
+    <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
+      Hello From Products
+    </div>
+  );
+};
+
+export default ProductPage;
