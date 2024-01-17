044d2751d27bffa5ddbd5eac794447ea4a621f12
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-01-17 17:16:35 +0300

    chore(ui): create  persitent accordion side-bar

--- app/(products)/products/_components/nav-item.tsx
+++ app/(products)/products/_components/nav-item.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
-import { ArrowDown } from "lucide-react";
 
 import {
   AccordionContent,
@@ -9,15 +9,19 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Checkbox } from "@/components/ui/checkbox";
+import { Skeleton } from "@/components/ui/skeleton";
 import { productConfig } from "@/config/products";
 import { cn } from "@/lib/utils";
 
-const NavItem = () => {
-  const router = useRouter();
-  const pathname = usePathname();
+interface NavItemProps {
+  onExpand: (id: string) => void;
+}
+
+const NavItem = ({ onExpand }: NavItemProps) => {
   return (
     <AccordionItem value={"categories"} className="border-none">
       <AccordionTrigger
+        onClick={() => onExpand("categories")}
         className={cn(
           "rounded-md bg-primary/20 p-2 text-black no-underline transition hover:no-underline",
         )}
@@ -27,13 +31,14 @@ const NavItem = () => {
       <AccordionContent className="ml-3 bg-white pt-2 text-black">
         <AccordionItem value="women" className="border-none">
           <AccordionTrigger
+            onClick={() => onExpand("women")}
             className={cn(
               "rounded-md bg-white p-2 text-black no-underline transition hover:no-underline",
             )}
           >
-            <span className="text-sm font-medium mt-2">Women</span>
+            <span className="mt-2 text-sm font-medium">Women</span>
           </AccordionTrigger>
-          <AccordionContent className="pt-2 text-neutral-700 space-y-4">
+          <AccordionContent className="space-y-4 pt-2 text-neutral-700">
             {productConfig.womenSidebarNav.map((categoryName, index) => (
               <div key={index} className="flex items-center">
                 <Checkbox className="mr-2 h-4 w-4" />
@@ -44,15 +49,16 @@ const NavItem = () => {
         </AccordionItem>
         <AccordionItem value="men" className="border-none">
           <AccordionTrigger
+            onClick={() => onExpand("men")}
             className={cn(
               "rounded-md bg-white p-2 text-black no-underline transition hover:no-underline",
             )}
           >
             <div className="flex items-center">
-              <p className="text-sm font-medium mt-2">Men</p>
+              <p className="mt-2 text-sm font-medium">Men</p>
             </div>
           </AccordionTrigger>
-          <AccordionContent className="pt-2 text-neutral-700 space-y-4">
+          <AccordionContent className="space-y-4 pt-2 text-neutral-700">
             {productConfig.menSidebarNav.map((categoryName, index) => (
               <div key={index} className="flex items-center">
                 <Checkbox className="mr-2 h-4 w-4" />
@@ -67,3 +73,23 @@ const NavItem = () => {
 };
 
 export default NavItem;
+
+interface NavItemSkeletonProps extends React.HTMLAttributes<HTMLElement> {}
+NavItem.Skeleton = function NavItemSkeleton({
+  className,
+  ...props
+}: NavItemSkeletonProps) {
+  return (
+    <div className={cn("", className)} {...props}>
+      <Skeleton className="w-[170px]">
+        <div className="mt-2 flex flex-col gap-y-4">
+          <Skeleton className="h-10 w-full" />
+          <Skeleton className="h-10 w-full" />
+          <Skeleton className="h-10 w-full" />
+          <Skeleton className="h-10 w-full" />
+          <Skeleton className="h-10 w-full" />
+        </div>
+      </Skeleton>
+    </div>
+  );
+};
--- app/(products)/products/_components/sidebar.tsx
+++ app/(products)/products/_components/sidebar.tsx
@@ -1,17 +1,64 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useLocalStorage } from "usehooks-ts";
 
 import { Accordion } from "@/components/ui/accordion";
 import { cn } from "@/lib/utils";
 
 import NavItem from "./nav-item";
 
+interface ProductSidebarProps extends React.HTMLAttributes<HTMLElement> {
+  storageKey?: string;
+}
+
 const ProductSidebar = ({
   className,
-}: React.HTMLAttributes<HTMLElement>) => {
+  storageKey = "t-sidebar-state",
+  ...props
+}: ProductSidebarProps) => {
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {},
+  );
+
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+
+      return acc;
+    },
+    [],
+  );
+
+  const onExpand = (id: string) => {
+    setExpanded((curr) => ({
+      ...curr,
+      [id]: !expanded[id],
+    }));
+  };
+
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
-    <div className={cn(className)}>
-      <Accordion type="multiple" className="space-y-2">
-        <NavItem />
-      </Accordion>
+    <div className={cn(className)} {...props}>
+      {isClient ? (
+        <Accordion
+          type="multiple"
+          defaultValue={defaultAccordionValue}
+          className="space-y-2"
+        >
+          <NavItem onExpand={onExpand} />
+        </Accordion>
+      ) : (
+        <NavItem.Skeleton />
+      )}
     </div>
   );
 };
