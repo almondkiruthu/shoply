3b7c87e7c828bfccf4877e17af0b9676269b0c22
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-01-20 18:17:45 +0300

    chore: create products pagination

--- app/(products)/products/_components/products.tsx
+++ app/(products)/products/_components/products.tsx
@@ -1,6 +1,9 @@
-import Image from "next/image";
-import Link from "next/link";
+"use client";
 
+import { useState } from "react";
+import Image from "next/image";
+
+import PaginationSection from "@/components/client-pagination";
 import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
 import { formatPrice } from "@/lib/format";
@@ -12,55 +15,71 @@ interface ProductsProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 const Products = ({ products, className, ...props }: ProductsProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage, setProductsPerPage] = useState(3);
+
+  const lastProductIndex = currentPage * productsPerPage;
+  const firstProductIndex = lastProductIndex - productsPerPage;
+  const currentProducts = products.slice(firstProductIndex, lastProductIndex);
+
   return (
-    <div
-      className={cn(
-        "mx-auto grid grid-cols-1 gap-y-8 lg:mx-0 lg:grid-cols-2 xl:grid-cols-3",
-        className,
-      )}
-      {...props}
-    >
-      {products.map((product) => (
-        <div
-          key={product.id}
-          className="w-[250px] space-y-2 rounded-lg border border-solid border-[0.9] 
+    <>
+      <div
+        className={cn(
+          "mx-auto grid grid-cols-1 gap-y-8 lg:mx-0 lg:grid-cols-2 xl:grid-cols-3",
+          className,
+        )}
+        {...props}
+      >
+        {currentProducts.map((product) => (
+          <div
+            key={product.id}
+            className="w-[250px] space-y-2 rounded-lg border border-solid border-[0.9] 
             bg-gradient-to-b from-white to-primary/[0.05] p-5 
           shadow-md sm:w-[300px]"
-        >
-          <div className="flex flex-col items-center justify-center p-5">
-            <Image src={product.image} alt={product.title} />
+          >
+            <div className="flex flex-col items-center justify-center p-5">
+              <Image src={product.image} alt={product.title} />
+            </div>
+            <div className="ml-4 flex flex-col gap-y-2 text-left">
+              <h3 className="font-sans_bold text-xl font-bold tracking-tight">
+                {product.title}
+              </h3>
+              <p className="text-sm font-normal leading-normal">
+                {formatPrice(product.price)}
+              </p>
+            </div>
+            <div className="flex items-center gap-x-5 md:ml-4">
+              {product.sizes.map((size, index) => (
+                <div key={index}>
+                  <Button
+                    key={index}
+                    variant="outline"
+                    size="icon"
+                    className="border-primary text-primary"
+                  >
+                    <p>{size.toUpperCase()}</p>
+                  </Button>
+                </div>
+              ))}
+            </div>
+            <div className="ml-4 flex items-center pt-4">
+              <Button className="">Add to Cart</Button>
+              <Button className="ml-auto mr-4 rounded-full" size="icon">
+                <Icons.heart className="h-4 w-4" />
+              </Button>
+            </div>
           </div>
-          <div className="ml-4 flex flex-col gap-y-2 text-left">
-            <h3 className="font-sans_bold text-xl font-bold tracking-tight">
-              {product.title}
-            </h3>
-            <p className="text-sm font-normal leading-normal">
-              {formatPrice(product.price)}
-            </p>
-          </div>
-          <div className="flex items-center gap-x-5 md:ml-4">
-            {product.sizes.map((size, index) => (
-              <div key={index}>
-                <Button
-                  key={index}
-                  variant="outline"
-                  size="icon"
-                  className="border-primary text-primary"
-                >
-                  <p>{size.toUpperCase()}</p>
-                </Button>
-              </div>
-            ))}
-          </div>
-          <div className="ml-4 flex items-center pt-4">
-            <Button className="">Add to Cart</Button>
-            <Button className="ml-auto mr-4 rounded-full" size="icon">
-              <Icons.heart className="h-4 w-4" />
-            </Button>
-          </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+      <PaginationSection
+        totalItems={products.length}
+        itemsPerPage={productsPerPage}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        className="my-8 mr-16"
+      />
+    </>
   );
 };
 
---
+++ components/client-pagination.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { cn } from "@/lib/utils";
+
+import { buttonVariants } from "./ui/button";
+
+interface PaginationSectionProps extends React.HTMLAttributes<HTMLDivElement> {
+  totalItems: any;
+  itemsPerPage: any;
+  currentPage: any;
+  setCurrentPage: any;
+}
+
+function PaginationSection({
+  className,
+  totalItems,
+  itemsPerPage,
+  currentPage,
+  setCurrentPage,
+  ...props
+}: PaginationSectionProps) {
+  const pages = [];
+  for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
+    pages.push(i);
+  }
+  const maxPageNum = 10; //Max Pages to display once
+  const pageNumLimit = Math.floor(maxPageNum / 2); // current page should be in the middle if possible
+
+  let activePages = pages.slice(
+    Math.max(0, currentPage - 1 - pageNumLimit),
+    Math.min(currentPage - 1 + pageNumLimit + 1, pages.length),
+  );
+
+  const handleNextPage = () => {
+    if (currentPage < pages.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  //Function to render page Numbers with ellipsis
+  const renderPages = () => {
+    const renderedPages = activePages.map((page, index) => (
+      <PaginationItem
+        key={index}
+        className={cn(
+          buttonVariants({
+            variant: "outline",
+            size: "icon",
+          }),
+          currentPage === page ? "rounded-md bg-primary text-white" : "",
+        )}
+      >
+        <PaginationLink onClick={() => setCurrentPage(page)}>
+          {page}
+        </PaginationLink>
+      </PaginationItem>
+    ));
+
+    // Add ellipsis at the start if necessary
+    if (activePages[0] > 1) {
+      renderedPages.unshift(
+        <PaginationEllipsis
+          key="ellipsis-start"
+          onClick={() => setCurrentPage(activePages[0] - 1)}
+        />,
+      );
+    }
+
+    // Add ellipsis at the end if necessary
+    if (activePages[activePages.length - 1] < pages.length) {
+      renderedPages.push(
+        <PaginationEllipsis
+          key="ellipsis-end"
+          onClick={() =>
+            setCurrentPage(activePages[activePages.length - 1] + 1)
+          }
+        />,
+      );
+    }
+
+    return renderedPages;
+  };
+
+  return (
+    <div className={cn(className)} {...props}>
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious onClick={handlePrevPage} />
+          </PaginationItem>
+          {renderPages()}
+          <PaginationItem>
+            <PaginationNext onClick={handleNextPage} />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </div>
+  );
+}
+
+export default PaginationSection;
