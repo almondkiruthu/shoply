74457317d5921fd3acac23d16573f3f411462461
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-01-20 23:52:00 +0300

    style: create cart site navigation

---
+++ app/(cart)/cart/_components/cart-site-nav.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
+const CartSiteNavigation = () => {
+  const pathname = usePathname();
+
+  return (
+    pathname.startsWith("/cart") && (
+      <div className="container pt-2 text-sm md:pt-8">
+        <div className="flex max-w-[150px] items-center gap-x-2">
+          <p className="text-primary">Home</p>
+          <p className="text-muted-foreground">/</p>
+          <p className="text-muted-foreground">Cart</p>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default CartSiteNavigation;
