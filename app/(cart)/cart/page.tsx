d050ff09307eb4e2c9e91f57745ce97c34a5586f
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-01-21 00:03:37 +0300

    chore: create cart page

---
+++ app/(cart)/cart/page.tsx
@@ -0,0 +1,11 @@
+interface CartPageProps {}
+
+const CartPage = () => {
+  return (
+    <>
+      <div>Let&apos;s Go This are Products added in the cart</div>
+    </>
+  );
+};
+
+export default CartPage;
