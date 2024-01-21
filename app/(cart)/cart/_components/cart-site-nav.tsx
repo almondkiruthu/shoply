3c97a9b13977bc61b139e06f3f329c04991612f1
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-01-21 04:00:31 +0300

    style: add margin to cart site navigator

--- app/(cart)/cart/_components/cart-site-nav.tsx
+++ app/(cart)/cart/_components/cart-site-nav.tsx
@@ -8,7 +8,7 @@ const CartSiteNavigation = () => {
   return (
     pathname.startsWith("/cart") && (
       <div className="container pt-2 text-sm md:pt-8">
-        <div className="flex max-w-[150px] items-center gap-x-2">
+        <div className="ml-4 flex max-w-[150px] items-center gap-x-2">
           <p className="text-primary">Home</p>
           <p className="text-muted-foreground">/</p>
           <p className="text-muted-foreground">Cart</p>
