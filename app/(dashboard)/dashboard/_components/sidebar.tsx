4bdf8ff8c87a6a3ba031f5756523ac469614b14b
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-01-27 14:29:32 +0300

    style: create sidebar routes

---
+++ app/(dashboard)/dashboard/_components/sidebar.tsx
@@ -0,0 +1,10 @@
+import { SidebarRoutes } from "./sidebar-routes";
+export const Sidebar = () => {
+  return (
+    <div className="flex h-full flex-col overflow-y-auto border-r bg-white shadow-sm">
+      <div className="flex w-full flex-col">
+        <SidebarRoutes />
+      </div>
+    </div>
+  );
+};
