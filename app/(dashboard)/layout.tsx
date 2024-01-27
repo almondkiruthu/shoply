63779a9a45c1368053090179360ce0428406bd73
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-01-27 14:18:43 +0300

    style: implement dash sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import DashBoardSiteNavigation from "./dashboard/_components/dashboard-site-nav";
+import { Sidebar } from "./dashboard/_components/sidebar";
 
 interface DashBoardProps {
   children?: React.ReactNode;
@@ -8,8 +9,14 @@ const DashBoard = ({ children }: DashBoardProps) => {
   return (
     <>
       <DashBoardSiteNavigation />
-      <div className="container">
-        <main className="mb-8 md:mb-10">{children}</main>
+      <div className="container h-full">
+        <aside
+          className="fixed inset-y-0 z-50 hidden h-full w-56 
+        flex-col md:mt-[4.05rem] md:flex"
+        >
+          <Sidebar />
+        </aside>
+        <main className="container mb-8 md:mb-10 md:pl-72">{children}</main>
       </div>
     </>
   );
