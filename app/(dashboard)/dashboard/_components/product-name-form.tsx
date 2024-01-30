bc83fd28996e6a6cf28d7e415e2b01573fb2c2ef
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-01-30 23:52:00 +0300

    chore: create product name form

---
+++ app/(dashboard)/dashboard/_components/product-name-form.tsx
@@ -0,0 +1,11 @@
+import { Product } from "@prisma/client";
+
+interface ProductNameFormProps {
+  data: Product;
+}
+
+const ProductNameForm = ({}: ProductNameFormProps) => {
+  return <div></div>;
+};
+
+export default ProductNameForm;
