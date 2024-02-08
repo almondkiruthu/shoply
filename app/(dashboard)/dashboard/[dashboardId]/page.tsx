7a2096c0857707bfd222d62b6082a4d16a5f4068
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-02-08 15:40:56 +0300

    ci: create product id page

---
+++ app/(dashboard)/dashboard/[dashboardId]/page.tsx
@@ -0,0 +1,42 @@
+import { Separator } from "@/components/ui/separator";
+import { db } from "@/lib/db";
+
+import ProductArrivialAndPopularForm from "./_components/product-arrival-and-popular-form";
+import ProductCategoryForm from "./_components/product-category-form";
+import ProductImageForm from "./_components/product-image-form";
+import ProductNameForm from "./_components/product-name-form";
+import ProductPriceForm from "./_components/product-price-form";
+import ProductQuantityForm from "./_components/product-quantity-form";
+import ProductSizeForm from "./_components/product-size-form";
+
+interface DashBoardIdPageProps {}
+
+const DashBoardIdPage = async ({}: DashBoardIdPageProps) => {
+  const sizes = await db.size.findMany({});
+  const suCategories = await db.subcategory.findMany({});
+  return (
+    <>
+      <div className="grid gap-6 md:mt-4 md:grid-cols-2">
+        <div className="flex">
+          <div className="flex flex-col items-start space-y-8">
+            <ProductNameForm />
+            <ProductPriceForm />
+            <ProductQuantityForm />
+            <ProductArrivialAndPopularForm />
+          </div>
+          <Separator orientation="vertical" className="ml-auto mr-4 h-full" />
+        </div>
+        {/* Tab 2 for the DashBoardIdPage  */}
+        <div>
+          <div className="flex flex-col items-start space-y-8">
+            <ProductSizeForm sizes={sizes} />
+            <ProductCategoryForm subCategories={suCategories} />
+            <ProductImageForm />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DashBoardIdPage;
