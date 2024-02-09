23350ade8d496d236eaa917e1473e192aae32106
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-02-10 02:14:42 +0300

    chore: create product-size update route

--- app/api/products/[productId]/route.ts
+++ app/api/products/[productId]/route.ts
@@ -10,16 +10,46 @@ export async function PATCH(
     const { productId } = params;
     const values = await req.json();
 
-    const course = await db.product.update({
-      where: {
-        id: productId,
-      },
+    // Check if sizes are provided in the request
+    if ("sizes" in values && Array.isArray(values.sizes)) {
+      const sizes = values.sizes.map((sizeId: string) => ({ id: sizeId }));
+
+      // Find the product by ID including all the current sizes
+      const existingProduct = await db.product.findUnique({
+        where: { id: productId },
+        include: { sizes: true },
+      });
+
+      // Disconnect existing sizes from the product
+      await db.product.update({
+        where: { id: productId },
+        data: {
+          sizes: {
+            disconnect: existingProduct?.sizes.map((size) => ({ id: size.id })),
+          },
+        },
+      });
+
+      // Connect new sizes to the product
+      await db.product.update({
+        where: { id: productId },
+        data: {
+          sizes: {
+            connect: sizes.map((size: { id: string }) => ({ id: size.id })),
+          },
+        },
+      });
+    }
+
+    // Update other fields if needed
+    const updatedProduct = await db.product.update({
+      where: { id: productId },
       data: {
-        ...values,
+        ...(values && { ...values, sizes: undefined }), // Exclude sizes from other updates
       },
     });
 
-    return NextResponse.json(course);
+    return NextResponse.json(updatedProduct);
   } catch (error) {
     console.log("[PRODUCT_ID]", error);
     return new NextResponse("Internal Error", { status: 500 });
---
+++ config/size-form.ts
@@ -0,0 +1,6 @@
+export const sizeMapping: Record<any, string> = {
+  "65bcc2c1a81db48713497bfe": "Small",
+  "65bcc2c1a81db48713497bff": "Medium",
+  "65bcc2c1a81db48713497c00": "Large",
+  "65bcc2c1a81db48713497c01": "Extra Large",
+};
