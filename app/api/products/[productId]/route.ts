67b60edb0709797de995d71b3c694fb678ed909f
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-02-11 23:04:55 +0300

    refactor: create api route for sizes

--- app/api/products/[productId]/route.ts
+++ app/api/products/[productId]/route.ts
@@ -10,42 +10,10 @@ export async function PATCH(
     const { productId } = params;
     const values = await req.json();
 
-    // Check if sizes are provided in the request
-    if ("sizes" in values && Array.isArray(values.sizes)) {
-      const sizes = values.sizes.map((sizeId: string) => ({ id: sizeId }));
-
-      // Find the product by ID including all the current sizes
-      const existingProduct = await db.product.findUnique({
-        where: { id: productId },
-        include: { sizes: true },
-      });
-
-      // Disconnect existing sizes from the product
-      await db.product.update({
-        where: { id: productId },
-        data: {
-          sizes: {
-            disconnect: existingProduct?.sizes.map((size) => ({ id: size.id })),
-          },
-        },
-      });
-
-      // Connect new sizes to the product
-      await db.product.update({
-        where: { id: productId },
-        data: {
-          sizes: {
-            connect: sizes.map((size: { id: string }) => ({ id: size.id })),
-          },
-        },
-      });
-    }
-
-    // Update other fields if needed
     const updatedProduct = await db.product.update({
       where: { id: productId },
       data: {
-        ...(values && { ...values, sizes: undefined }), // Exclude sizes from other updates
+        ...values,
       },
     });
 
