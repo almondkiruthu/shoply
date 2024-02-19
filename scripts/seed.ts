8a85e5067d750a9dbebd3b7fa6bb5900cbe7c836
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-02-19 22:59:48 +0300

    refactor: change prisma db and seed data

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -4,68 +4,21 @@ const database = new PrismaClient();
 
 async function main() {
   try {
-    const existingSizesCount = await database.size.count();
-
-    let womenCategory = await database.category.findFirst({
-      where: { name: "Women" },
-    });
-
-    // If it doesn't exist, create the category with two subcategories
-    if (!womenCategory) {
-      womenCategory = await database.category.create({
-        data: {
-          name: "Women",
-          subcategories: {
-            create: [
-              { name: "Shirts" },
-              { name: "Dresses" },
-              { name: "Shorts" },
-              { name: "Skirts" },
-              { name: "Sports Clothing" },
-            ],
-          },
-        },
-      });
-    }
-
-    // Check if the 'Men' category already exists
-    let menCategory = await database.category.findFirst({
-      where: { name: "Men" },
-    });
-
-    // If it doesn't exist, create the category with two subcategories
-    if (!menCategory) {
-      menCategory = await database.category.create({
-        data: {
-          name: "Men",
-          subcategories: {
-            create: [
-              { name: "Shirts" },
-              { name: "Polos" },
-              { name: "Pants" },
-              { name: "Sports Clothing" },
-            ],
-          },
-        },
-      });
-    }
-
-    console.log({ womenCategory, menCategory });
-
-    if (existingSizesCount > 0 && existingSizesCount === 4) {
-      console.log("Data already seeded.");
-      return;
+    const existingSubCategoryCount = await database.subcategory.count();
+    if (existingSubCategoryCount >= 7) {
+      return console.log("Data is already seeded!");
     } else {
-      await database.size.createMany({
+      await database.subcategory.createMany({
         data: [
-          { name: "Small" },
-          { name: "Medium" },
-          { name: "Large" },
-          { name: "Extra Large" },
+          { name: "Shirts" },
+          { name: "Dresses" },
+          { name: "Shorts" },
+          { name: "Skirts" },
+          { name: "Sports Clothing" },
+          { name: "Polos" },
+          { name: "Pants" },
         ],
       });
-
-      console.log("Data seeded successfully.");
     }
   } catch (error) {
     console.error("Error seeding the database sizes", error);
