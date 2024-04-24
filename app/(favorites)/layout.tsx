aa2f28e34f58b5e2357f03b6c007f0284f92ec95
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-04-24 20:35:54 +0300

    chore: add site-header component in cart

--- app/(favorites)/layout.tsx
+++ app/(favorites)/layout.tsx
@@ -1,4 +1,5 @@
 import SiteFooter from "@/components/site-footer";
+import SiteHeader from "@/components/site-header";
 
 import FavoritesSiteNavigation from "./favorites/_components/favorites-site-nav";
 
@@ -9,6 +10,7 @@ interface FavoritesLayoutProps {
 const FavoritesLayout = ({ children }: FavoritesLayoutProps) => {
   return (
     <>
+      <SiteHeader />
       <FavoritesSiteNavigation />
       <div className="container flex-1">
         <main className="mx-auto mb-8 max-w-[1040px] md:mb-10">{children}</main>
