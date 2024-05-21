9f553955118954c000ab3da80c8fb4ef29b36bba
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-05-21 23:48:31 +0300

    chore: fix indentation

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -8,7 +8,7 @@ const DashBoardPage = async ({}: DashBoardPageProps) => {
       <div
         className="container flex flex-1 flex-col items-center 
         bg-gradient-to-tr from-white/10 via-primary/10 to-slate-100 shadow-lg 
-       md:my-10 md:py-10"
+        md:my-10 md:py-10"
       >
         <ProductNameForm />
       </div>
