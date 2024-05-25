32e6e7e72a59323c45ca9799dcafe5250c924de5
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-05-25 20:02:02 +0300

    chore: remove unused files

--- actions/create-product/type.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-import { ActionState } from "@/lib/create-safe-action";
-import { Product } from "@prisma/client";
-
-import { CreateProduct } from "./schema";
-
-export type InputType = z.infer<typeof CreateProduct>;
-export type ReturnType = ActionState<InputType, Product>;
