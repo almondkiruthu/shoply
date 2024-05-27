bce3bcd9070f2e8fa5194b8e209793b33054afb5
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-05-27 19:19:33 +0300

    refactor: optimize fetch products function

--- data/product.ts
+++ data/product.ts
@@ -1,33 +1,29 @@
+import { notFound } from "next/navigation";
+
 import { db } from "@/lib/db";
 
 export const getAllProducts = async ({ query }: { query?: string }) => {
-  try {
-    let products = db.product.findMany({
+  let products = db.product.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  if (query) {
+    products = db.product.findMany({
+      where: {
+        OR: [
+          { name: { contains: query, mode: "insensitive" } },
+          { mainCategory: { contains: query, mode: "insensitive" } },
+        ],
+      },
       orderBy: {
         createdAt: "desc",
       },
     });
-
-    if (query) {
-      products = db.product.findMany({
-        where: {
-          OR: [
-            { name: { contains: query, mode: "insensitive" } },
-            { mainCategory: { contains: query, mode: "insensitive" } },
-          ],
-        },
-        orderBy: {
-          createdAt: "desc",
-        },
-      });
-    }
-
-    return products;
-  } catch (error) {
-    throw new Error(
-      "Something went wrong☹️ on our end or we don't have such a product",
-    );
   }
+
+  return products;
 };
 
 export const getProductsCount = async () => {
